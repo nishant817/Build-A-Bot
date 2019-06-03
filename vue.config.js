f9fe3106844742be49058b99a5ece0ffa5cda372
Nishant <nishant817@outlook.com>
2019-06-02 19:42:01 -0500

    pointed api url to azure

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
    devServer: {
       proxy: {
          '/api': {
-            target: 'http://localhost:8081',
+            //target: 'http://localhost:8081',
+            target: 'https://buildabotserver.azurewebsites.net',
             changeOrigin: true, //webpac config to avoid cors error
          },
       },
